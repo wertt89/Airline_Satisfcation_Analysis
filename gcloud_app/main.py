3034256c53036398cf9803a6b5a16030431299ee
jmcleary26 <jacquelyncleary@gmail.com>
2021-05-24 18:38:43 -0400

    Added gcloud deploy

---
+++ gcloud_app/main.py
@@ -0,0 +1,27 @@
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
