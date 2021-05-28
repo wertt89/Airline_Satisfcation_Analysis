6c8c17a78e5166e8e00c0f3172eeea5b661931ad
Colin <colinfoneill@gmail.com>
2021-05-27 19:34:47 -0500

    added code for flask app to main.py

--- gcloud_app/main.py
+++ gcloud_app/main.py
@@ -1,8 +1,28 @@
-
-from flask import Flask, render_template
+import os
+import pandas as pd
+import numpy as np
+import pickle
+from flask import Flask, render_template, request
+import joblib
 
 app = Flask(__name__)
 
+#check if file is already on disk, and if it is not, download from amazon S3
+loaded_model = pickle.load(open("random_forest.pkl", 'rb'))
+
+#https://colin-week22-data-test.s3.us-east-2.amazonaws.com/random_forest.pkl
+#requests.get of this url and saving to disk
+
+def ValuePredictor(to_predict_list):
+    to_predict = np.array(to_predict_list).reshape(1,-1)
+    
+    result = loaded_model.predict(to_predict)
+    proba = loaded_model.predict_proba(to_predict)
+    print(result)
+    print(proba)
+    print(loaded_model.classes_)
+    print(to_predict)
+    return result[0]
 
 @app.route('/')
 @app.route('/home')
@@ -17,9 +37,24 @@ def dashboard():
 def about():
     return render_template('about.html')
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == "POST":
+        to_predict_list = request.form.to_dict()
+        to_predict_list = list(to_predict_list.values())
+        to_predict_list = list(map(float, to_predict_list))
+        result = ValuePredictor(to_predict_list)
+        prediction = str(result)
+        return render_template("predict.html", prediction=prediction)
+
+    
+
+
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
+
+
 
 
 if __name__ == '__main__':
