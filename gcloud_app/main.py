79304d0a15a765ea4619b4347d100d38909147dd
Colin <colinfoneill@gmail.com>
2021-06-02 15:45:07 -0500

    added analysis html file

--- gcloud_app/main.py
+++ gcloud_app/main.py
@@ -17,7 +17,7 @@ if os.path.exists("random_forest.pkl"):
     print("file exists")
 
 else:
-    url = 'https://colin-week22-data-test.s3.us-east-2.amazonaws.com/random_forest.pkl'
+    url = 'https://nu-final-project.s3.us-east-2.amazonaws.com/random_forest.pkl'
     r = requests.get(url, allow_redirects=True)
     open('random_forest.pkl', 'wb').write(r.content)
     
@@ -47,10 +47,15 @@ def predict():
     if request.method == "POST":
         to_predict_list_dict = request.form.to_dict()
         to_predict_list_values = list(to_predict_list_dict.values())
+        print(to_predict_list_values)
         to_predict_list_float = list(map(float, to_predict_list_values))
         result = ValuePredictor(to_predict_list_float)
         prediction = str(result)
         return render_template("predict.html", prediction=prediction)
+
+@app.route('/analysis')
+def analysis():
+    return render_template('analysis.html')
 
     
 
